d4a00756020d077e02b2ba64e3743a909429b0af
bradc <bradcha85@gmail.com>
2019-05-04 06:50:27 +0900

    mysql 모듈추가 및 연동테스트

--- fontServer.js
+++ fontServer.js
@@ -64,7 +64,27 @@ app.post('/fontUpload', upload.single("fontUpload"), function(req, res, next) {
 	res.send(result); 
 
     
-    });
+	});
+	
+	var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  port     : 3306,
+  database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT * from Persons', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.', err);
+});
+
+connection.end();
 
 
 
