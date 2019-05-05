091956abc3b5e16aae4e761a437d40de9eec38b7
bradc <bradcha85@gmail.com>
2019-05-05 09:29:00 +0900

    로그인기능 기본적인 뼈대 완성 및 내정보보기 구현중

--- fontServer.js
+++ fontServer.js
@@ -56,12 +56,19 @@ app.get('/',function(req,res){
 	 
 	//if문을 써서 분기를 나누자.. 세션이 있으면 유저정보와 함께 ejs로 렌더링하고 
 	// 없으면 html로 렌더링
-	if (req.session.user){ 
-		console.log("session"); 
-		res.render("../main", req.session.user);
-	};
-        res.sendFile(__dirname +'/main.html');
-	 });
+	if(req.session.user){ 
+		 console.log("session exist"); 
+		 res.render("../main", req.session.user);
+		 }else{ 
+			//res.sendFile(__dirname +'/main.html');
+			res.render("../main",{
+				mail: '',
+				pw: '',
+				authorized: true
+			});
+		 				
+		}
+  });
 	 
 app.get('/admin',function(req,res){
         res.sendFile(__dirname +'/adminPage.html')
@@ -82,17 +89,16 @@ app.post('/fontUpload', upload.single("fontUpload"), function(req, res, next) {
 
 	res.send(result); 
 
-    
 	});
 
-	
 
-	app.post('/session', function(req, res){
-
-		var paramID = req.body.mail || req.query.mail;
+	app.post('/login', function(req, res){
+		
+    console.log("req : " , req)
+		var mail = req.body.mail || req.query.mail;
 		var pw = req.body.pw || req.query.pw;
 		
-		console.log(paramID);
+		console.log(mail);
 		console.log(pw);
 
 		if(req.session.user){
@@ -103,41 +109,64 @@ app.post('/fontUpload', upload.single("fontUpload"), function(req, res, next) {
 			res.redirect("/");
 		}else{
 			//request에 세션 내용이 없을 경우
-			req.session.user =
-			{
-					mail: paramID,
-					pw: pw,
-					authorized: true
-			};
-			res.render("../main",req.session.user);
-		
 
+			let mysql      = require('mysql');
+			let connection = mysql.createConnection({
+				host     : 'localhost',
+				user     : 'root',
+				password : 'root',
+				port     : 3306,
+				database : 'my_db'
+			});
+			connection.connect();
+			
+			let sql = 'SELECT password FROM USERS WHERE mail = "'+mail+'"';
+			connection.query(sql, function(err, rows, fields) {
+				if (!err){
+					console.log('result: ', rows);
+					console.log('result2 : ', rows[0].password);
+
+					if (rows[0].password == pw){
+						console.log("correct password");
+						req.session.user ={
+																mail: mail,
+																pw: pw,
+																authorized: true
+															};
+						//res.render("../main",req.session.user);
+						res.send({"isSuccess" : "true"});
+						//res.redirect("/");
+					}else{
+						console.log("wrong password");
+						res.send({"isSuccess" : "false"});
+					}
+				}
+				else{
+					console.log('Error while performing Query.', err);
+				}
+			});
+
+			connection.end();
 		}
 	
-	//	res.send('count: '+req.session.count);
+	 app.get('/logout',function(req,res){
+			req.session.destroy();
+			res.redirect('/');
+	 });
+
+	 app.get('/my', function(req, res){
+      console.log("myInfo : " ,req.session.user);
+	 });
+
 	
 	});
 
 	
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  port     : 3306,
-  database : 'my_db'
-});
 
-connection.connect();
 
-connection.query('SELECT * from Persons', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.', err);
-});
 
-connection.end();
+
+
 
 
 
